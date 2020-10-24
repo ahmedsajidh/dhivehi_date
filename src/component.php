b5a378312b00b7e6db884a2a8e776a2044ad726c
ahmed sajidh <ahmedsajidh@gmail.com>
2020-10-25 00:01:19 +0500

    dhivehidate

---
+++ src/component.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sajidh\Dhivehidate;
+
+class Component{
+
+
+    public function thaana_date($secs){
+
+        $bit = array(
+            ' އަހަރު'        => $secs / 31556926 % 12,
+            ' ހަފްތާ'        => $secs / 604800 % 52,
+            ' ދުވަސް'        => $secs / 86400 % 7,
+            ' ގަޑިއިރު'        => $secs / 3600 % 24,
+            ' މިނެޓް'    => $secs / 60 % 60,
+            ' ސިކުންތު'    => $secs % 60
+        );
+    
+        foreach($bit as $x => $y){
+            if($y > 1)$ret[] =   $y.$x;
+            if($y == 1)$ret[] =  $y.$x;
+        }
+//        array_splice($ret, count($ret)-1, 0, 'އަދި');
+        $ret[] = 'ކުރިން';
+    
+        return join(' ', $ret);
+    }
+}
--- src/dhivehidate.php
+++ src/dhivehidate.php
@@ -1,8 +1,16 @@
-<? php
+<?php 
 
 namespace Sajidh\Dhivehidate;
 
-class dhivehidate{
+use Sajidh\Dhivehidate\Component;
 
-    
+/**
+ * undocumented class
+ */
+class Dhivehidate extends Component
+{
+    public function thaanadate($oldtime) {
+        $ctime = time() - strtotime($oldtime);
+        return $this->thaana_date($ctime);  
+    }
 }
